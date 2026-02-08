53c34f02a84aeab8ace5944ea3d5ae1847db6a97
Bibek Tamang <bibek7here@gmail.com>
2026-02-09 00:12:32 +0545

    update prisma config

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,2 +1,12 @@
-console.log("Hello via Bun!");
-export * from './generated/prisma/index';
+import { PrismaClient } from "./prisma/generated/prisma/index.js";
+export * from "./prisma/generated/prisma/index.js";
+export { PaymentStatus } from "./prisma/generated/prisma/index.js";
+export { Prisma } from "./prisma/generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = new PrismaClient({ adapter });
+export { PrismaClient, prisma };
