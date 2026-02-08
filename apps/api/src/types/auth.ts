f7ba18aedc17d133520c2121a9b799b7d3fd8b07
Bibek Tamang <bibek7here@gmail.com>
2026-02-08 22:29:32 +0545

    add: authenticated interface

---
+++ apps/api/src/types/auth.ts
@@ -0,0 +1,6 @@
+import type { Request } from "express";
+import type { UserResponseDto } from "src/modules/user/dto/UserResponseDto";
+
+export interface AuthenticatedRequest extends Request {
+	user: UserResponseDto;
+}
