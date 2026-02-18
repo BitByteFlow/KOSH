56941e48de6f3626cbfac38a38f379a95cb63b93
Bibek Tamang <bibek7here@gmail.com>
2026-02-19 00:24:41 +0545

    add: report metrics gql init

--- apps/web/modules/reports/components/ReportMetrics.tsx
+++ apps/web/modules/reports/components/ReportMetrics.tsx
@@ -3,20 +3,32 @@ import { useAnalyticsMetrics } from "../hooks/useReports";
 import { getDateRange } from "@/lib/date-utils";
 import { AnalyticsMetricCard } from "./AnalyticsMetric";
 import { MetricCardSkeleton } from "@/components/MetricCardSkeletion";
+import { useQuery } from "@apollo/client/react";
+import { gql } from "@/gql";
+
+
+const GET_REPORT_METRICS = gql(`
+	query getReportData ($startDate: String!, $endDate: String!){
+		getAnalyticsMetrics (startDate: $startDate, endDate: $endDate) {
+			label
+			value
+			trend
+			trendLabel
+			isPositive
+			subtitle
+		}
+	}
+`)
 
 const ReportMetrics = ({ dateRange }: { dateRange: string }) => {
 	const { startDate, endDate } = useMemo(
 		() => getDateRange(dateRange),
 		[dateRange],
 	);
-	console.log("dates :", startDate, endDate);
-	const {
-		data: metrics,
-		isPending,
-		isError,
-	} = useAnalyticsMetrics(startDate, endDate);
 
-	if (isPending) {
+	const { data: metrics, loading, error } = useQuery(GET_REPORT_METRICS, { variables: { startDate, endDate } })
+
+	if (loading) {
 		return (
 			<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
 				<MetricCardSkeleton />
@@ -26,13 +38,13 @@ const ReportMetrics = ({ dateRange }: { dateRange: string }) => {
 			</div>
 		);
 	}
-	if (isError) {
+	if (error) {
 		return <h2>error</h2>;
 	}
 
 	return (
 		<div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-			{metrics?.map((metric) => (
+			{metrics?.getAnalyticsMetrics?.map((metric) => (
 				<AnalyticsMetricCard
 					key={metric.label}
 					label={metric.label}
